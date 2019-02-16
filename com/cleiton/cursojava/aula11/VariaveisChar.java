3001f23e360796e0826c3c5e9475724c5c2bec03
Cleiton Dsd <s.diascleiton@gmail.com>
2019-02-16 16:45:55 -0200

    Aula 11 - Tipos de variáveis | Class 10 Type of variables

---
+++ com/cleiton/cursojava/aula11/VariaveisChar.java
@@ -0,0 +1,14 @@
+package com.cleiton.cursojava.aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		//char i = 'i'; 
+		//char o = 'o';
+		
+		char i = 105; 
+		char o = 111;	
+		System.out.println(""+o+i); //Adicionando Aspas duplas e "+", printa "oi"
+	}								//Caso oposto soma os valores dos caracter (tabela ASCII)
+}
---
+++ com/cleiton/cursojava/aula11/VariavesPontoFlutuante.java
@@ -0,0 +1,14 @@
+package com.cleiton.cursojava.aula11;
+
+public class VariavesPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double valorPassagem = 4.00;
+		float  valorTomate = 3.95f;
+		
+		System.out.println("Valor da passagem = "+ valorPassagem);
+		System.out.println("Valor do tomate = "+ valorTomate);		
+	}
+
+}
