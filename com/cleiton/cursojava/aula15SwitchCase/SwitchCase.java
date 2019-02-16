2f285f4628ee47621913e994fc3ac47eefe632bd
Cleiton Dsd <s.diascleiton@gmail.com>
2019-02-16 16:57:50 -0200

    Aula 15 | Class 15 - Switch Case

---
+++ com/cleiton/cursojava/aula15SwitchCase/SwitchCase.java
@@ -0,0 +1,53 @@
+package com.cleiton.cursojava.aula15SwitchCase;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Insira um número do dia da Semana: ");
+		byte diaSemana = scan.nextByte();
+		
+		/** if (diaSemana == 1) {
+			System.out.println("Domingo");
+		}
+		else if (diaSemana == 2) {
+			System.out.println("Segunda");
+		}
+		else if (diaSemana == 3) {
+			System.out.println("Terça");
+		}
+		else if (diaSemana == 4) {
+			System.out.println("Quarta");
+		}
+		else if (diaSemana == 5) {
+			System.out.println("Quinta");
+		}
+		else if (diaSemana == 6) {
+			System.out.println("Sexta");
+		}
+		else if (diaSemana == 7) {
+			System.out.println("Sábado");
+		}
+		else {
+			System.out.println("Não é um dia da semana valido, uma semana contém 7 dias.");
+		}
+		**/
+		switch(diaSemana) {
+		case 1 : System.out.println("Domingo"); break;
+		case 2 : System.out.println("Segunda"); break;
+		case 3 : System.out.println("Terça"); break;
+		case 4 : System.out.println("Quarta"); break;
+		case 5 : System.out.println("Quinta"); break;
+		case 6 : System.out.println("Sexta"); break;
+		case 7 : System.out.println("Sábado"); break;
+		default : System.out.println("Não é um dia da semana valido, uma semana contém 7 dias.");
+		
+		}
+		
+	}
+
+}
