8f795d9bcefb344016e3993704af09f65b9f6146
Cleiton Dsd <s.diascleiton@gmail.com>
2019-02-16 16:36:34 -0200

    Aula 10 - Variáveis | Class 10 - Variables

---
+++ com/cleiton/cursojava/aula10/Variaveis.java
@@ -0,0 +1,16 @@
+package com.cleiton.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade =17;
+		String nome= ("Cleiton");
+		
+		System.out.println ("Seu nome é "+ nome + " e tem "+ idade + " anos");
+		
+
+	}
+
+} 
+ 
