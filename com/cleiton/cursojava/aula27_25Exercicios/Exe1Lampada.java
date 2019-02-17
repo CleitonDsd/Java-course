a56c5a22964f2fbc71c2cb63b1cdf8675185672e
Cleiton Dsd <s.diascleiton@gmail.com>
2019-02-17 12:57:40 -0300

    Acrescentando variável booleana para verificar estado da lâmpada -

--- com/cleiton/cursojava/aula27_25Exercicios/Exe1Lampada.java
+++ com/cleiton/cursojava/aula27_25Exercicios/Exe1Lampada.java
@@ -4,21 +4,24 @@ public class Exe1Lampada {
 
 	// Classe do exercicio número 1 - Lampadas
 
+	boolean ligada;
+
 	String ligarLampada() {
 		System.out.println(" ON ");
+		ligada = true;
 		System.out.println("metódo 'ligarLampada' \n");
-		
-		return "Lampada Ligada";
+		return "Lampada Ligada " + ligada;
 
 	}
 
 	String desligarLampada() {
 		System.out.println("");
-		
+
 		System.out.println(" OFF ");
+		ligada = false;
 		System.out.println("metódo 'desligarLampada' \n");
 
-		return "Lampada Desligada";
+		return "Lampada Desligada " + ligada;
 	}
 
 }
