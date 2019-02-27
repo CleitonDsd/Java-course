7787f059d148c130c3dabc2bbffeb315934f2574
Cleiton Dsd <s.diascleiton@gmail.com>
2019-02-27 15:29:42 -0300

    aula 28 - Pacotes | class 28 - packages

---
+++ com/cleiton/cursojava/aula28Pacotes/Pacotes.java
@@ -0,0 +1,23 @@
+package com.cleiton.cursojava.aula28Pacotes;
+
+import com.cleiton.cursojava.aula16e17exercicios.Exe13BaseExpoente;
+import com.cleiton.cursojava.aula25OOMetodosSimples.Carro;
+
+public class Pacotes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//importa classe carro da aula 25
+		Carro carro;
+		
+		//importa classe do exe13BaseExpoente da aula 16 e 17 (exercicios)	
+		Exe13BaseExpoente baseExp;
+		
+		
+		//PAcotes separam a classes conforme seu propósito, organizando grupos funcionais
+
+	}
+
+}
