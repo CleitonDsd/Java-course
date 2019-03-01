3191708c3c43e51c8d8cfcac3f21393d7d6b5e6a
Cleiton Dsd <s.diascleiton@gmail.com>
2019-03-01 16:35:10 -0300

    Aula 20 | Class 20 - This

--- com/cleiton/cursojava/aula20Mexercicios/Exe06JogoDaVelhaComMatrizes.java
+++ com/cleiton/cursojava/aula20Mexercicios/Exe06JogoDaVelhaComMatrizes.java
@@ -5,118 +5,95 @@ import java.util.Scanner;
 public class Exe06JogoDaVelhaComMatrizes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		/*
-		 *  Faça um programa para jogar jogo da velha.
-		 * 
-		 * O programa deve permitir que dois jogadores façam uma partida de jogo da
-		 * velha, usando o pc para ver o tabuleiro. Cada jogador vai informando
-		 * alternadamente a posição onde deseja colocar a sua peça ("X" e "O"). O
-		 * programa deve impedir jogadas inválidas e determinar automaticamente que o
-		 * jogo terminou e quem foi o vencedor (jogador1 ou jogador2). A cada nova
-		 * jogada, o programa deve atualizar a situação do tabuleiro na tela.
-		 * 
-		 */
 
 		Scanner scan = new Scanner(System.in);
+
 		char[][] jogoVelha = new char[3][3];
 
 		System.out.println("Jogador 1 = X");
 		System.out.println("Jogador 2 = O");
 
 		boolean ganhou = false;
-		int jogadas = 1; // Vai contar em qual jogada o jogo está;
+		int jogada = 1;
 		char sinal;
 		int linha = 0, coluna = 0;
+
 		while (!ganhou) {
-			if (jogadas % 2 == 1) { // jogador1
-				System.out.println("Vez do jogador 1. Escolha linha e coluna (1-3). ");
+
+			if (jogada % 2 == 1) { // jogador 1
+
+				System.out.println("Vez do jogador 1. Escolha linha e coluna (1-3).");
 				sinal = 'X';
 			} else {
-				System.out.println("Vez do jogador 2. Escolha linha e coluna (1-3). ");
+
+				System.out.println("Vez do jogador 2. Escolha linha e coluna (1-3).");
 				sinal = 'O';
 			}
 
 			boolean linhaValida = false;
 			while (!linhaValida) {
-				System.out.println("Digite uma linha válida (1, 2 ou 3)");
+				System.out.println("Entre com a linha (1, 2 ou 3)");
 				linha = scan.nextInt();
 				if (linha >= 1 && linha <= 3) {
 					linhaValida = true;
 				} else {
-					System.out.println("Entrada inválida, digite novamente: ");
+					System.out.println("Entrada inválida, tente novamente");
 				}
 			}
+
 			boolean colunaValida = false;
 			while (!colunaValida) {
-				System.out.println("Digite uma coluna válida (1, 2 ou 3)");
+				System.out.println("Entre com a coluna (1, 2 ou 3)");
 				coluna = scan.nextInt();
 				if (coluna >= 1 && coluna <= 3) {
 					colunaValida = true;
 				} else {
-					System.out.println("Entrada inválida, digite novamente: ");
+					System.out.println("Entrada inválida, tente novamente");
 				}
 			}
+
 			linha--;
 			coluna--;
 			if (jogoVelha[linha][coluna] == 'X' || jogoVelha[linha][coluna] == 'O') {
-				System.out.println("Posição já utilizada, tente novamente");
-			} else { // Jogada válida
+				System.out.println("Posição já usada, tente novamente");
+			} else { // jogada válida
 				jogoVelha[linha][coluna] = sinal;
-				jogadas++;
+				jogada++;
 			}
-			// Imprimir o tabuleiro
-			
-			//Usando FOR para imprimir resultados
-			
+
+			// imprimir tabuleiro
 			for (int i = 0; i < jogoVelha.length; i++) {
 				for (int j = 0; j < jogoVelha[i].length; j++) {
-					System.out.println(jogoVelha[i][j] + " || ");
+					System.out.print(jogoVelha[i][j] + " | ");
 				}
-				System.out.println("");
+				System.out.println();
 			}
-			
-			//Usando for Each para imprimir resultados
-			
-			
-			for(char [] vetorJogoVelha : jogoVelha) {
-				for( char jogo : vetorJogoVelha) {
-					System.out.println(jogo + " || ");
-				}
-			}
-			
-			
-			// verificar se tem ganhador
-			if ((jogoVelha[0][0] == 'X' && jogoVelha[0][1] == 'X' && jogoVelha[0][2] == 'X')// linha1
-					|| (jogoVelha[1][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[1][2] == 'X') // linha2
-					|| (jogoVelha[2][0] == 'X' && jogoVelha[2][1] == 'X' && jogoVelha[2][2] == 'X')// linha3
-					|| (jogoVelha[0][0] == 'X' && jogoVelha[1][0] == 'X' && jogoVelha[2][0] == 'X')// colunas1
-					|| (jogoVelha[0][1] == 'X' && jogoVelha[0][1] == 'X' && jogoVelha[0][1] == 'X') // colunas2
-					|| (jogoVelha[0][2] == 'X' && jogoVelha[0][2] == 'X' && jogoVelha[0][2] == 'X')// colunas3
-					|| (jogoVelha[0][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][2] == 'X') // Diagonais
-					|| (jogoVelha[0][2] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][0] == 'X')) { // DiagonaisInversas
 
+			// verifica se tem ganhador
+			if ((jogoVelha[0][0] == 'X' && jogoVelha[0][1] == 'X' && jogoVelha[0][2] == 'X') || // linha 1
+					(jogoVelha[1][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[1][2] == 'X') || // linha 2
+					(jogoVelha[2][0] == 'X' && jogoVelha[2][1] == 'X' && jogoVelha[2][2] == 'X') || // linha 3
+					(jogoVelha[0][0] == 'X' && jogoVelha[1][0] == 'X' && jogoVelha[2][0] == 'X') || // coluna 1
+					(jogoVelha[0][1] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][1] == 'X') || // coluna 2
+					(jogoVelha[0][2] == 'X' && jogoVelha[1][2] == 'X' && jogoVelha[2][2] == 'X') || // coluna 3
+					(jogoVelha[0][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][2] == 'X') || // diagonal
+					(jogoVelha[0][2] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][0] == 'X')) { // diagonal inversa
 				ganhou = true;
-				System.out.println("Parabéns pela vitória jogador 1! ");
-			} else if ((jogoVelha[0][0] == 'O' && jogoVelha[0][1] == 'O' && jogoVelha[0][2] == 'O')// linha1
-					|| (jogoVelha[1][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[1][2] == 'O') // linha2
-					|| (jogoVelha[2][0] == 'O' && jogoVelha[2][1] == 'O' && jogoVelha[2][2] == 'O')// linha3
-					|| (jogoVelha[0][0] == 'O' && jogoVelha[1][0] == 'O' && jogoVelha[2][0] == 'O')// colunas1
-					|| (jogoVelha[0][1] == 'O' && jogoVelha[0][1] == 'O' && jogoVelha[0][1] == 'O') // colunas2
-					|| (jogoVelha[0][2] == 'O' && jogoVelha[0][2] == 'O' && jogoVelha[0][2] == 'O')// colunas3
-					|| (jogoVelha[0][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][2] == 'O') // Diagonais
-					|| (jogoVelha[0][2] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][0] == 'O')) { // DiagonaisInversas
-
+				System.out.println("Parabéns, jogador 1 ganhou!");
+			} else if ((jogoVelha[0][0] == 'O' && jogoVelha[0][1] == 'O' && jogoVelha[0][2] == 'O') || // linha 1
+					(jogoVelha[1][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[1][2] == 'O') || // linha 2
+					(jogoVelha[2][0] == 'O' && jogoVelha[2][1] == 'O' && jogoVelha[2][2] == 'O') || // linha 3
+					(jogoVelha[0][0] == 'O' && jogoVelha[1][0] == 'O' && jogoVelha[2][0] == 'O') || // coluna 1
+					(jogoVelha[0][1] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][1] == 'O') || // coluna 2
+					(jogoVelha[0][2] == 'O' && jogoVelha[1][2] == 'O' && jogoVelha[2][2] == 'O') || // coluna 3
+					(jogoVelha[0][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][2] == 'O') || // diagonal
+					(jogoVelha[0][2] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][0] == 'O')) { // diagonal inversa
 				ganhou = true;
-				System.out.println("Parabéns pela vitória jogador 2! ");
-
-			} else if (jogadas > 9) {
+				System.out.println("Parabéns, jogador 2 ganhou!");
+			} else if (jogada > 9) {
 				ganhou = true;
-
-				System.out.println("DEU VELHA!!! Partida Empatada!!!");
+				System.out.println("Ninguém ganhou essa partida.");
 			}
 		}
-
 	}
 }
---
+++ com/cleiton/cursojava/aula30This/Principal.java
@@ -0,0 +1,13 @@
+package com.cleiton.cursojava.aula30This;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Carro van = new Carro("Fiat", "Ducato");
+
+		System.out.println(van.numPassageiros);
+	}
+
+}
